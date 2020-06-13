4c4bc7dd29af6ac7f8610008c959d3970018021a
Eliza Regas <elizaregas@gmail.com>
2020-06-13 18:43:58 -0400

    making js notes

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,127 +1,66 @@
-$('document').ready(function() {
-
+$("document").ready(function () {
+  // global variables
   var hour = [9, 10, 11, 12, 13, 14, 15, 16, 17];
   var scheduleColumn;
 
+  // for loop to generate divs for each work hour
   for (i = 0; i < hour.length; i++) {
     createTimeSlots(i, hour[i]);
   }
 
-function createTimeSlots(i, hour) {
-  var currentHour = moment().format('k');
-  var displayHour = hour > 12 ? hour - 12 : hour;
-  var hourColumn = $("<div class='col-sm-2 timeCol'></div>").text(displayHour + ":00");
-  scheduleColumn = $(`<div class='col-sm-8 schedule' id='${hour + "a"}'><input class="inputText"></div>`);
-  var saveColumn = $(`<div class="col-sm-2" id="save"><button type="button" class="btn btn-info" id='${hour + "b"}'><i class="fas fa-save"></i></button></div>`)
-  var newRow = $(`<div class='row' id='${hour}'></div>`).append(hourColumn, scheduleColumn, saveColumn);
-  $('.container').append(newRow);
+  // function to create a div for each work hour
+  function createTimeSlots(i, hour) {
+    // getting the current hour from moment.js
+    var currentHour = moment().format("k");
+    // changing the way the hour is displayed for accuracy after noon
+    var displayHour = hour > 12 ? hour - 12 : hour;
+    // variables to generate the columns for each row
+    var hourColumn = $("<div class='col-sm-2 timeCol'></div>").text(
+      displayHour + ":00"
+    );
+    scheduleColumn = $(
+      `<div class='col-sm-8 schedule' id='${
+        hour + "a"
+      }'><input class="inputText"></div>`
+    );
+    var saveColumn = $(
+      `<div class="col-sm-2" id="save"><button type="button" class="btn btn-info" id='${
+        hour + "b"
+      }'><i class="fas fa-save"></i></button></div>`
+    );
+    var newRow = $(`<div class='row' id='${hour}'></div>`).append(
+      hourColumn,
+      scheduleColumn,
+      saveColumn
+    );
+    $(".container").append(newRow);
 
-  if (hour == currentHour) {
-    $("#" + hour).css("background-color","red");
-  } else if (hour < currentHour) {
-    $("#" + hour).css("background-color","#d3d3d3");
-  } else {
-    $("#" + hour).css("background-color","green");
+    if (hour == currentHour) {
+      $("#" + hour).css("background-color", "red");
+    } else if (hour < currentHour) {
+      $("#" + hour).css("background-color", "#d3d3d3");
+    } else {
+      $("#" + hour).css("background-color", "green");
+    }
+
+    var savedText = window.localStorage.getItem(hour + "a");
+    console.log(savedText);
+    if (savedText !== null) {
+      $("#" + hour + "a input").val(savedText);
+    }
   }
 
-  var savedText = window.localStorage.getItem(hour + "a");
-  console.log(savedText);
-  if (savedText !== null) {
-    $('#' + hour + 'a input').val(savedText);
+  function displayCurrentDate() {
+    var currentDate = moment().format("dddd, MMMM Do, YYYY");
+    $("#currentDay").text(currentDate);
   }
-}
+  displayCurrentDate();
 
-function displayCurrentDate() {
-  var currentDate = moment().format('dddd, MMMM Do, YYYY');
-  $('#currentDay').text(currentDate);
-}
-displayCurrentDate();
-
-// WHEN I click into a timeblock
-// THEN I can enter an event
-
-// $('.schedule').on('click', function() {
-//   $(this).append('<input class="inputText">');
-//   $(this).off('click');  
-// })
-
-$('.btn').on('click', function() {
-  var buttonId = $(this).attr('id');
-  var textId = buttonId.replace('b', 'a');
-  var textContent = $('#' + textId).children()[0].value;
-  //setting items in the local storage
-  localStorage.setItem(textId, textContent);
-})
-
-
-
-
-
-
-
-
-
-
-
-// WHEN I click the save button for that timeblock
-// THEN the text for that event is saved in local storage
-
-// WHEN I refresh the page
-// THEN the saved events persist
-
-// function init() {
-//   // Get stored todos from localStorage
-//   // Parsing the JSON string to an object
-//   var storedSchedule = JSON.parse(localStorage.getItem("todos"));
-
-//   // If todos were retrieved from localStorage, update the todos array to it
-//   if (storedTodos !== null) {
-//     todos = storedTodos;
-//   }
-
-//   // Render todos to the DOM
-//   renderTodos();
-// }
-
-// function storeTodos() {
-//   // Stringify and set "todos" key in localStorage to todos array
-//   localStorage.setItem("todos", JSON.stringify(todos));
-// }
-
-// // When form is submitted...
-// todoForm.addEventListener("submit", function(event) {
-//   event.preventDefault();
-
-//   var todoText = todoInput.value.trim();
-
-//   // Return from function early if submitted todoText is blank
-//   if (todoText === "") {
-//     return;
-//   }
-
-//   // Add new todoText to todos array, clear the input
-//   todos.push(todoText);
-//   todoInput.value = "";
-
-//   // Store updated todos in localStorage, re-render the list
-//   storeTodos();
-//   renderTodos();
-// });
-
-// // When a element inside of the todoList is clicked...
-// todoList.addEventListener("click", function(event) {
-//   var element = event.target;
-
-//   // If that element is a button...
-//   if (element.matches("button") === true) {
-//     // Get its data-index value and remove the todo element from the list
-//     var index = element.parentElement.getAttribute("data-index");
-//     todos.splice(index, 1);
-
-//     // Store updated todos in localStorage, re-render the list
-//     storeTodos();
-//     renderTodos();
-//   }
-// });
-
-})
+  $(".btn").on("click", function () {
+    var buttonId = $(this).attr("id");
+    var textId = buttonId.replace("b", "a");
+    var textContent = $("#" + textId).children()[0].value;
+    //setting items in the local storage
+    localStorage.setItem(textId, textContent);
+  });
+});
